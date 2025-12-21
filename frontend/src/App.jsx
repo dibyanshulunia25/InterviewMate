3a5379ccb3d594f240da86ec1785491027f94869
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2025-12-21 10:40:50 +0530

    Landing Page Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import {BrowserRouter as Router, Routes , Route} from 'react-router-dom'
+import {Toaster} from 'react-hot-toast'
+import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Home/Dashboard'
+import InterviewPrep from './pages/InterviewPrep/Interviewprep'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Router>
+      <Routes>
+        {/* Default Route */}
+        <Route path='/' element={<LandingPage />} />
+
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/interview-prep/:sessionId' element={<InterviewPrep />} />
+      </Routes>
+    </Router>
+
+    <Toaster
+      toastOptions={{
+        className:"",
+        style:{
+          fontSize:"13px",
+        },
+      }}
+    />
+    </>
   )
 }
 
