923f589add4ac3639edcaee5825b35f5513626bc
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2026-01-01 19:36:37 +0530

    created dashboard layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
-import LandingPage from './pages/LandingPage'
-import Dashboard from './pages/Home/Dashboard'
-import InterviewPrep from './pages/InterviewPrep/Interviewprep'
+import LandingPage from './Pages/LandingPage'
+import Dashboard from './Pages/Home/Dashboard'
+import InterviewPrep from './Pages/InterviewPrep/Interviewprep'
 import UserProvider from './Context/UserContext'
 
 const App = () => {
---
+++ frontend/src/Components/layouts/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import ProfileInfoCard from '../Cards/ProfileInfoCard'
+
+const Navbar = () => {
+  return (
+    <div className='h-20 bg-white border-b border-gray-200/50 backdrop-blur-[100px] py-2.5 px-4 md:px-0 sticky top-1 z-200'>
+        <div className='container px-4 mx-auto flex items-center justify-between gap-5'>
+            <Link to="/dashboard">
+                <h2 className='text-lg md:text-xl font-medium leading-5'>InterviewMate</h2>
+            </Link>
+
+            <ProfileInfoCard />
+        </div>
+    </div>
+)
+}
+
+export default Navbar
