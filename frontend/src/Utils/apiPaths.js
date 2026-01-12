1a20c2fbf4e98d97ba9a2fbd87f2f8e30300e64b
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2026-01-12 11:42:11 +0530

    added vite_api_url to communicate the frontend to the deployed backend

--- frontend/src/Utils/apiPaths.js
+++ frontend/src/Utils/apiPaths.js
@@ -1,31 +1,31 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 export const API_PATHS = {
-    AUTH:{
+    AUTH: {
         REGISTER: "/api/auth/register", //Signup
         LOGIN: "/api/auth/login", //Authenticate user and return JWT token
         GET_PROFILE: "/api/auth/profile", //Get logged-in user profile
     },
 
-    IMAGE:{
+    IMAGE: {
         UPLOAD_IMAGE: "/api/auth/upload-image", //Upload image
     },
 
-    AI:{
+    AI: {
         GENERATE_QUESTIONS: "/api/ai/generate-questions", //Generate interview questions
         GENERATE_EXPLANATION: "/api/ai/generate-explanation", //Generate concept explanation   
     },
 
-    SESSION:{
+    SESSION: {
         CREATE: "/api/sessions/create", //Create session
-        GET_ALL : "/api/sessions/my-sessions", //Get session
-        GET_ONE: (id)=> `/api/sessions/${id}`, //Get session by id
-        DELETE: (id)=> `/api/sessions/${id}`, //Delete session by id
+        GET_ALL: "/api/sessions/my-sessions", //Get session
+        GET_ONE: (id) => `/api/sessions/${id}`, //Get session by id
+        DELETE: (id) => `/api/sessions/${id}`, //Delete session by id
     },
 
-    QUESTION:{
+    QUESTION: {
         ADD_TO_SESSION: "/api/questions/add", //Add question to session
-        PIN: (id)=> `/api/questions/${id}/pin`, //Pin/unpin question
-        UPDATE_NOTE: (id)=> `/api/questions/${id}/note`, //Update/add note
+        PIN: (id) => `/api/questions/${id}/pin`, //Pin/unpin question
+        UPDATE_NOTE: (id) => `/api/questions/${id}/note`, //Update/add note
     }
 };
