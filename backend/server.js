c290189fa9d29297a25c9b428376428909543cf5
dibyanshulunia25 <dibyanshulunia098@gmail.com>
2026-01-13 11:23:54 +0530

    minor bug fixes

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Handle SPA routing: serve index.html for any unknown route
-app.get("*", (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
